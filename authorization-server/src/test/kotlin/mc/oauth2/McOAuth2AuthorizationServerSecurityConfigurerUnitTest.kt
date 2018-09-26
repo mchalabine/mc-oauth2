3d1264399e7632ae607416cc036fe4d3cb56eb99
Mikhail Chalabine <mc00x1@gmail.com>
2018-09-26 15:35:16 +0200

    Further tests

--- authorization-server/src/test/kotlin/mc/oauth2/McOAuth2AuthorizationServerSecurityConfigurerUnitTest.kt
+++ authorization-server/src/test/kotlin/mc/oauth2/McOAuth2AuthorizationServerSecurityConfigurerUnitTest.kt
@@ -1,5 +1,6 @@
 package mc.oauth2
 
+import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
@@ -8,7 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.ApplicationContext
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication
+import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.test.web.servlet.MockMvc
@@ -17,12 +21,8 @@ import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
 import org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup
 import org.springframework.web.context.WebApplicationContext
 import javax.servlet.Filter
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*
-import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.*
 
 
-/**
- */
 @RunWith(SpringRunner::class)
 @SpringBootTest(classes = [(McOAuth2AuthorizationServerSecurityConfigurer::class)])
 @WebAppConfiguration
@@ -54,6 +54,13 @@ class McOAuth2AuthorizationServerSecurityConfigurerUnitTest {
         val authenticationToken = UsernamePasswordAuthenticationToken("user", "password")
         val authenticate = authenticationManager.authenticate(authenticationToken)
         assertTrue(authenticate.isAuthenticated)
+    }
+
+    @Test(expected = BadCredentialsException::class)
+    fun testAuthenticationManagerRejectsAsExpectedWhereUserMatchesNot() {
+        val authenticationToken = UsernamePasswordAuthenticationToken("user1", "password")
+        val authenticate = authenticationManager.authenticate(authenticationToken)
+        assertFalse(authenticate.isAuthenticated)
     }
 
     @Test
