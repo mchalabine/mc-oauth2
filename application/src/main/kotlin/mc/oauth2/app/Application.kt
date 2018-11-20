ef0627d15c6b999ccead9710384e4bc8885054f7
Mikhail Chalabine <mc00x1@gmail.com>
2018-11-20 08:02:32 +0100

    #2 Minor cleanup

--- application/src/main/kotlin/mc/oauth2/app/Application.kt
+++ application/src/main/kotlin/mc/oauth2/app/Application.kt
@@ -1,16 +1,16 @@
 package mc.oauth2.app
 
-import mc.oauth2.config.McOAuth2AppConfiguration
+import mc.oauth2.config.AppConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.context.annotation.Import
 
 @SpringBootApplication
-@Import(McOAuth2AppConfiguration::class)
-class McOAuth2() {
+@Import(AppConfiguration::class)
+class Application() {
 
     fun main(args: Array<String>) {
-        SpringApplicationBuilder(McOAuth2::class.java).run(*args)
+        SpringApplicationBuilder(Application::class.java).run(*args)
     }
 }
 
--- application/src/test/kotlin/mc/oauth2/app/ApplicationUnitTest.kt
+++ application/src/test/kotlin/mc/oauth2/app/ApplicationUnitTest.kt
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
-@SpringBootTest(classes = [(McOAuth2::class)])
-class McOAuth2UnitTest {
+@SpringBootTest(classes = [(Application::class)])
+class ApplicationUnitTest {
 
     @Test
     fun testRunning() {
