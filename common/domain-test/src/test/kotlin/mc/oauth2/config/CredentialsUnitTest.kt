d1074a376f7709f7edde4e037d272b90ff294e80
Mikhail Chalabine <mc00x1@gmail.com>
2018-11-25 16:29:59 +0100

    Minor cleanup

--- common/domain-test/src/test/kotlin/mc/oauth2/config/CredentialsUnitTest.kt
+++ common/domain-test/src/test/kotlin/mc/oauth2/config/CredentialsUnitTest.kt
@@ -3,7 +3,7 @@ package mc.oauth2.config
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-internal class CredentialsUnit {
+internal class CredentialsUnitTest {
 
     @Test
     fun testToStringReturnsExpected() {
