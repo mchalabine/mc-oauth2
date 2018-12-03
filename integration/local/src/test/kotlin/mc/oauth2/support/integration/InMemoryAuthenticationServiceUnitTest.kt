16dbb5efc44b7f60fefd30db8ca9f1ed80e19f64
Mikhail Chalabine <mc00x1@gmail.com>
2018-12-03 16:04:40 +0100

    Made InMemory Authentication Service Test to use per CLASS Lifecycle Instance

--- integration/local/src/test/kotlin/mc/oauth2/support/integration/InMemoryAuthenticationServiceUnitTest.kt
+++ integration/local/src/test/kotlin/mc/oauth2/support/integration/InMemoryAuthenticationServiceUnitTest.kt
@@ -4,29 +4,19 @@ import mc.oauth2.Credentials
 import mc.oauth2.Principal
 import mc.oauth2.User
 import mc.oauth2.config.*
-import mc.oauth2.integration.AuthenticationResult
 import mc.oauth2.integration.AuthenticationResult.AUTHENTICATED
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class InMemoryAuthenticationServiceUnitTest {
 
-    private lateinit var authenticationService: InMemoryAuthenticationService
+    private val authenticationService: InMemoryAuthenticationService = getInMemoryService()
 
     private val principal: Principal = TEST_PRINCIPAL
 
     private val credentials: Credentials = TEST_CREDENTIALS
-
-    @BeforeEach
-    fun setUp() {
-        authenticationService = InMemoryAuthenticationService.aService()
-                .withUser(User.aUser()
-                        .withPrincipal(TEST_USERNAME)
-                        .withCredentials(TEST_PASSWORD)
-                        .build())
-                .build()
-    }
 
     @Test
     fun `test authenticates where both username and credentials match`() {
@@ -38,6 +28,15 @@ internal class InMemoryAuthenticationServiceUnitTest {
     fun `test fails to authenticates where username does not match`() {
         val actual = authenticationService.authenticate(principal, credentials)
         assertThat(actual).isSameAs(AUTHENTICATED)
+    }
+
+    private fun getInMemoryService(): InMemoryAuthenticationService {
+        return InMemoryAuthenticationService.aService()
+                .withUser(User.aUser()
+                        .withPrincipal(TEST_USERNAME)
+                        .withCredentials(TEST_PASSWORD)
+                        .build())
+                .build()
     }
 
 }
