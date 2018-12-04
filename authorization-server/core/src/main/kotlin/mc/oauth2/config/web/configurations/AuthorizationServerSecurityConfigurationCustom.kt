9f5297b62f7921a22e904f994dbd86f2622d8f6a
Mikhail Chalabine <mc00x1@gmail.com>
2018-12-04 21:17:51 +0100

    Minor doc fix

--- authorization-server/core/src/main/kotlin/mc/oauth2/config/web/configurations/AuthorizationServerSecurityConfigurationCustom.kt
+++ authorization-server/core/src/main/kotlin/mc/oauth2/config/web/configurations/AuthorizationServerSecurityConfigurationCustom.kt
@@ -3,8 +3,13 @@ package mc.oauth2.config.web.configurations
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerSecurityConfiguration
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
 
 /**
+ * Allows to override the default Spring Authorization Server Security set by the
+ * [EnableAuthorizationServer] configuration annotation.
+ *
+ * @see EnableAuthorizationServer
  * @author Michael Chalabine
  */
 @Configuration
