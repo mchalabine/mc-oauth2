2df0bbeb0ca84560e2a37de5ee981c8e4d9fdf79
Mikhail Chalabine <mc00x1@gmail.com>
2018-12-07 09:42:14 +0100

    Added AuthorizationServerConfiguration unit test skeleton

---
+++ authorization-server/core/src/test/kotlin/mc/oauth2/configurations/AuthorizationServerConfigurationUnitTest.kt
@@ -0,0 +1,9 @@
+package mc.oauth2.configurations
+
+import org.junit.jupiter.api.Assertions.*
+
+/**
+ * @author Michael Chalabine
+ * @since
+ */
+internal class AuthorizationServerConfigurationUnitTest
